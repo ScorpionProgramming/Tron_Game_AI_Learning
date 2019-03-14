869b4a5dfa47be97762a91bfb883413382806076
matrixpill <35921614+FenryHord@users.noreply.github.com>
2019-03-14 12:55:24 +0100

    Collision mit erstem element gefixed

--- game.js
+++ game.js
@@ -32,7 +32,14 @@ class TRONGame {
     }
 
     addPlayer(color) {
-        let p1 = new Player(2, 2, "south", this.field, color);
+        const positions = [{ x: 0, y: 0, dir: "east" }, { x: 31, y: 31, dir: "west" }];
+        let position = {};
+        if (this.field[positions[0].x][positions[0].y].placed === 0)
+            position = positions[0];
+        else
+            position = positions[1];
+
+        let p1 = new Player(position.x, position.y, position.dir, this.field, color);
         return this.players.push(p1) - 1;
     }
 
@@ -41,10 +48,7 @@ class TRONGame {
     }
 
     start() {
-
-        //let p2 = new Player(canvas.height-2, 2, "north");
-        //let p3 = new Player(2, canvas.height-2,  "west");
-        //let p4 = new Player(canvas.height-2, canvas.height-2, "EAST");
+        this.then = Date.now();
 
         // Background image
         var bgReady = false;
@@ -55,12 +59,7 @@ class TRONGame {
         bgImage.src = "images/background.png";
 
 
-        //test
-        /*  let posX = 0;
-         let x = 0;
-         let factor = 1; */
 
-        //var then = 0;
         //starts the game -----------
 
 
@@ -69,9 +68,9 @@ class TRONGame {
     }
 
     /**
- * Positions data = data on GRID = canvas.size / tilesize
- * x y are the pixel on the canvas
- */
+    * Positions data = data on GRID = canvas.size / tilesize
+    * x y are the pixel on the canvas
+    */
 
 
 
@@ -85,38 +84,15 @@ class TRONGame {
         }
     }
     update(deltatime) {
-        //console.log(deltatime);
-
-        //wired stuff happened
-        /* if (deltatime > interval) {
-            deltatime = 0;
-        } */
-
-        //console.log(1/deltatime); //at the moment ~60 fps
-
-
-        //....
-        /* if (x > 512) {
-            factor = -1;
-        } else if (x < 0) {
-            factor = 1;
-        }
-
-        x += this.tileSize * factor; //3 blöcke a 32 px pro sec
-
-        posX = parseInt(x / this.tileSize) * this.tileSize; */
 
         this.calcdistance();
 
-        this.players.forEach(p => p.update(deltatime))
-        //p1.update(deltatime);
-        //p2.update(deltatime);
-        //p3.update(deltatime);
-        //p4.update(deltatime);
+        this.players.forEach(p => p.update(deltatime));
     }
 
     //drawfunktionen und so ...
     render() {
+
         const { tileSize, field, canvasSize } = this;
 
         this.ctx.clearRect(0, 0, canvasSize, canvasSize);
@@ -129,19 +105,18 @@ class TRONGame {
         this.ctx.fillStyle = "brown";
         for (let i = 0; i < canvasSize / tileSize; i++) {
             for (let j = 0; j < canvasSize / tileSize; j++) {
-                //if (field[i][j].placed === 1) {
                 this.ctx.fillStyle = field[i][j].color;
                 this.ctx.fillRect(i * tileSize, j * tileSize, tileSize, tileSize);
-                //}
+
             }
         }
 
-        /* this.ctx.fillStyle = "red";
-        this.ctx.fillRect(0, 0 + posX, tileSize, tileSize); */
-
         this.players.forEach(p => {
-            this.ctx.fillStyle = "red";
-            this.ctx.fillRect(p.posX * tileSize, p.posY * tileSize, tileSize/2, tileSize/2)
+            this.ctx.fillStyle = "grey";
+            const x = p.posX * tileSize;
+            const y = p.posY * tileSize;
+            const w = tileSize / 2
+            this.ctx.fillRect(x, y, w, w);
         });
     }
 
@@ -155,8 +130,6 @@ class TRONGame {
 
 
         if (delta >= this.interval) {
-            /* console.timeEnd("render");
-            console.time("render"); */
             this.update(delta);
 
             this.render();
@@ -166,56 +139,6 @@ class TRONGame {
 
         requestAnimationFrame(this.gameLoop);
     }
-    //-----------------------------------------------------------------------------
-
-    /* doKeyDown(e) {
-        let key = e.keyCode;
-        console.log(key);
-        //player1 rot
-        //Keys: W A S D
-        //Keycodes: 87 65 83 68
-        const p1 = this.players[0];
-        switch (key) {
-            case 87:
-                console.log("W pressed");
-                p1.moveUp();
-                break;
-            case 65:
-                console.log("A pressed");
-                p1.moveLeft();
-                break;
-            case 83:
-                console.log("S pressed");
-                p1.moveDown();
-                break;
-            case 68:
-                console.log("D pressed");
-                p1.moveRight();
-                break;
-            default:
-                break;
-        }
-
-        if (key == 87) {
-            //console.log("W wurde gedrückt");
-        }
-
-        if (key = 65) {
-
-        }
-
-        //player2 blau
-        //keys: I J K L
-        //keycodes:
-
-        //player3 grün
-        //keys: up left down right
-        //keycodes: 38 37 40 39
-
-        //player4 gelb
-        //keys: kp_8 kp_4 kp_5 kp_6
-        //keycodes: 104 100 101 102
-    } */
 }
 
 
@@ -230,10 +153,9 @@ class Player {
         this.nextDir;
         this.field = field;
         this.color = color;
-        /*   this.x = this.posX;
-          this.y = this.posY; */
-
-        console.log(this.posX + " " + this.posY + " " + this.dir);//+ " " + this.x + " " + this.y);
+        field[posX][posY].placed = 1;
+        field[posX][posY].color = color;
+        console.log(this.posX + " " + this.posY + " " + this.dir);
     }
 
 
@@ -282,11 +204,7 @@ class Player {
     }
 
     update(deltatime) {
-        /* this.x = this.posX * tileSize;
-        this.y = this.posY * tileSize; */
         this.checkDirection();
-        // let speed = tileSize;//* deltatime;
-        //console.log(speed + " "+ deltatime);
 
         let nX = 0, nY = 0;
         if (this.dir === "east") {
@@ -311,20 +229,17 @@ class Player {
 
             console.log("moving");
 
-            this.field[this.posX][this.posY].placed = 1;
-            this.field[this.posX][this.posY].color = this.color;
+            this.field[nextPosX][nextPosY].placed = 1;
+            this.field[nextPosX][nextPosY].color = this.color;
 
             this.posX = nextPosX;
             this.posY = nextPosY;
 
-            // f
+
         } else { // here collision
             console.log("BOOOOOOOM");
         }
 
-        //auf position des objekts mappen
-        //this.posX = parseInt(this.x);
-        //this.posY = parseInt(this.y);
 
 
         //console.log(this.posX + "  " + this.posY + "  " + this.x + "  " + this.y + "  " + speed);
